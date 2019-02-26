f1417067589cf3a90f8110cb92b35a32b403be25
Meghna Agarwal <meghna@softtrends.local>
2019-02-26 12:47:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+﻿import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+var styles={color:'blue'}
+var styles1={color:'red'}
+class Card extends React.Component {
+    render(){
+        return (
+          <div className="card">
+          <h1>  <p style={styles1}>{this.props.name}</p></h1>
+          </div>
+      );
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // fires before component is mounted
+    constructor(props) {
+        
+        // makes this refer to this component
+        super(props);
+
+        // set local state
+        this.state = {
+            name: "Sudharani"
+        };
+
+    }
+
+    render() {
+        setTimeout(()=>{this.setState({name:'rani'})},5000);
+        const {name} = this.state; 
+        return (
+          <div>  
+            <h1 style={styles}>This Computer belongs to:</h1>
+            <Card name={name}> </Card>
+          </div>
+      )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
